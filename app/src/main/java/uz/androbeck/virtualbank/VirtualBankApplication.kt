f70986df3bd4ebac90833a20d70cad6c55ea57a6
Azizbek <abdusattorovazizbek571@mail.ru>
2024-07-17 19:15:10 +0300

    App file changed

--- app/src/main/java/uz/androbeck/virtualbank/VirtualBankApplication.kt
+++ app/src/main/java/uz/androbeck/virtualbank/VirtualBankApplication.kt
@@ -2,7 +2,7 @@ package uz.androbeck.virtualbank
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
-
+//Hello Virtual Bank
 @HiltAndroidApp
 class VirtualBankApplication : Application() {
 }
