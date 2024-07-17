1a9f1fb9d7e54307f476d3f1bbb2f847e59c05e2
AndroidDeveloperSanjar <sanjarkarimov1996@mail.ru>
2024-07-17 19:46:09 +0500

    Setup App

---
+++ app/src/main/java/uz/androbeck/virtualbank/MainActivity.kt
@@ -0,0 +1,14 @@
+package uz.androbeck.virtualbank
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
