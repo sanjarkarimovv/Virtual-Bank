af266438af11e11155954017ab6b8c954080b985
sanjarkarimov <sanjarkarimov1996@mail.ru>
2024-07-19 18:57:13 +0500

    Some things changed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,13 +42,12 @@ android {
 }
 
 dependencies {
-
+    implementation(libs.kotlin.stdlib)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    implementation(libs.androidx.security.crypto.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -76,5 +75,8 @@ dependencies {
     implementation(libs.view.binding)
 
     //Security
-    implementation(libs.security)
+    implementation(libs.androidx.security.crypto.ktx)
+
+    //MaskedEditText
+    implementation(libs.input.redmadrobot)
 }
