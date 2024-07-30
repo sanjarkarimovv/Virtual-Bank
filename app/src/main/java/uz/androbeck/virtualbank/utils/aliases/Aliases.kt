228c305f519f4b72a5037046c4fc845bbba5f3bf
AndroidDeveloperSanjar <sanjarkarimov1996@mail.ru>
2024-07-30 10:18:56 +0500

    Added custom loading button

---
+++ app/src/main/java/uz/androbeck/virtualbank/utils/aliases/Aliases.kt
@@ -0,0 +1,9 @@
+package uz.androbeck.virtualbank.utils.aliases
+
+import uz.androbeck.virtualbank.R
+
+typealias ColorRes = R.color
+typealias DrawableRes = R.drawable
+typealias StringRes = R.string
+typealias IdRes = R.id
+typealias FontRes = R.font
