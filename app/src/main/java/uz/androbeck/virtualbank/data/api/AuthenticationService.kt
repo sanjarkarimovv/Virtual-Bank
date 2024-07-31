3d9b72a6223b982a62a946c330ab73e3bdfb0f2e
Abdulfaiz <addwinsta@gmail.com>
2024-07-31 11:29:06 +0500

    add URL Sign In Verify

--- app/src/main/java/uz/androbeck/virtualbank/data/api/AuthenticationService.kt
+++ app/src/main/java/uz/androbeck/virtualbank/data/api/AuthenticationService.kt
@@ -3,6 +3,7 @@ package uz.androbeck.virtualbank.data.api
 import retrofit2.http.Body
 import retrofit2.http.POST
 import uz.androbeck.virtualbank.data.dto.common.response.TokenResDto
+import uz.androbeck.virtualbank.data.dto.request.SignInVerifyReqDto
 import uz.androbeck.virtualbank.data.dto.request.SignUpReqDto
 import uz.androbeck.virtualbank.utils.Constants
 
@@ -11,4 +12,10 @@ interface AuthenticationService {
     suspend fun signUp(
         @Body request: SignUpReqDto
     ): TokenResDto
+
+    @POST(Constants.Endpoint.SIGN_IN_VERIFY)
+    suspend fun signInVerify(
+        @Body request: SignInVerifyReqDto
+    ): TokenResDto
+
 }
--- app/src/main/java/uz/androbeck/virtualbank/data/source/remote/AuthenticationRemoteDataSourceImpl.kt
+++ app/src/main/java/uz/androbeck/virtualbank/data/source/remote/AuthenticationRemoteDataSourceImpl.kt
@@ -2,6 +2,7 @@ package uz.androbeck.virtualbank.data.source.remote
 
 import kotlinx.coroutines.flow.flow
 import uz.androbeck.virtualbank.data.api.AuthenticationService
+import uz.androbeck.virtualbank.data.dto.request.SignInVerifyReqDto
 import uz.androbeck.virtualbank.data.dto.request.SignUpReqDto
 import javax.inject.Inject
 
@@ -10,5 +11,9 @@ class AuthenticationRemoteDataSourceImpl @Inject constructor(
 ) : AuthenticationRemoteDataSource {
     override fun signUp(request: SignUpReqDto) = flow {
         emit(service.signUp(request))
+    }
+
+    override fun signInVerify(request: SignInVerifyReqDto) = flow {
+        emit(service.signInVerify(request))
     }
 }
