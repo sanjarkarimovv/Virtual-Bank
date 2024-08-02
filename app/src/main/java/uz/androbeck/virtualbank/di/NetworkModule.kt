4a8fcb810594db381d5948dfb3a824c4ae23212f
ELYOR <elyorxalilov9201@gmail.com>
2024-08-02 15:47:25 +0500

    elyor commit

--- app/src/main/java/uz/androbeck/virtualbank/di/NetworkModule.kt
+++ app/src/main/java/uz/androbeck/virtualbank/di/NetworkModule.kt
@@ -11,7 +11,7 @@ import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Converter
+import retrofit2.Converter 
 import retrofit2.Retrofit
 import uz.androbeck.virtualbank.BuildConfig
 import uz.androbeck.virtualbank.data.api.AuthenticationService
@@ -79,6 +79,7 @@ object NetworkModule {
     }
 
     @Provides
+
     @Singleton
     fun provideAuthService(
         retrofit: Retrofit
