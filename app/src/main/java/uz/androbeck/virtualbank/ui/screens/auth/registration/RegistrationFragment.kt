96112487fbb3e14b3c0883dd6cc7efc9954c1894
sanjarkarimov <sanjarkarimov1996@mail.ru>
2024-08-05 18:06:10 +0500

    Some things changed

--- app/src/main/java/uz/androbeck/virtualbank/ui/screens/auth/registration/RegistrationFragment.kt
+++ app/src/main/java/uz/androbeck/virtualbank/ui/screens/auth/registration/RegistrationFragment.kt
@@ -5,8 +5,10 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import by.kirich1409.viewbindingdelegate.viewBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 import uz.androbeck.virtualbank.R
 import uz.androbeck.virtualbank.databinding.FragmentRegistrationBinding
 import uz.androbeck.virtualbank.domain.ui_models.authentication.SignUpReqUIModel
@@ -16,8 +18,6 @@ import uz.androbeck.virtualbank.ui.base.BaseFragment
 import uz.androbeck.virtualbank.ui.screens.MainSharedViewModel
 import uz.androbeck.virtualbank.utils.extentions.toast
 import javax.inject.Inject
-
-// +99899-856-42-50
 
 //Hello Virtual Bank
 @AndroidEntryPoint
@@ -38,18 +38,27 @@ class RegistrationFragment : BaseFragment(R.layout.fragment_registration) {
     }
 
     override fun clicks(): Unit = with(binding) {
-        btnSignUp.onClickProgress(isProgress = false) {
-            accessSignUp()
+//        btnSignUp.onClickProgress(isProgress = false) {
+//            accessSignUp()
+//        }
+
+        btnSignUp.onClick = {
+            println(":::AAA clicked")
+            btnSignUp.isProgress = true
+            lifecycleScope.launch {
+                delay(5000L)
+                btnSignUp.isProgress = false
+            }
         }
     }
 
     private fun accessSignUp() = with(binding) {
-        val firstName = etFirstName.text?.toString().orEmpty()
-        val lastName = etLastName.text?.toString().orEmpty()
-        val password = etPassword.text?.toString().orEmpty()
-        val confirmPassword = etConfirmPassword.text?.toString().orEmpty()
-        val phoneNumber = etPhoneNumber.text?.toString().orEmpty()
-        val bornDate = etDate.text?.toString().orEmpty()
+        val firstName = etFirstName.editText.text?.toString().orEmpty()
+        val lastName = etLastName.editText.text?.toString().orEmpty()
+        val password = etPassword.editText.text?.toString().orEmpty()
+        val confirmPassword = etConfirmPassword.editText.text?.toString().orEmpty()
+        val phoneNumber = etPhoneNumber.editText.text?.toString().orEmpty()
+        val bornDate = etDate.editText.text?.toString().orEmpty()
         val gender = if (cbGender.isChecked) 0 else 1
         val requestModel = SignUpReqUIModel(
             firstName = firstName,
@@ -71,20 +80,20 @@ class RegistrationFragment : BaseFragment(R.layout.fragment_registration) {
                         return@onEach
                     }
                     if (isAccess) {
-                        btnSignUp.buttonState(isProgress = true)
-                        requestModel?.let(vm::signUp)
+//                        btnSignUp.buttonState(isProgress = true)
+//                        requestModel?.let(vm::signUp)
                     }
                 }.launchIn(this)
 
                 signUpEvent.onEach {
-                    btnSignUp.buttonState(isProgress = false)
+//                    btnSignUp.buttonState(isProgress = false)
                     requireActivity().onBackPressedDispatcher.onBackPressed()
                 }.launchIn(this)
             }
         }
 
         messageController.observeMessage().onEach {
-            btnSignUp.buttonState(isProgress = false)
+//            btnSignUp.buttonState(isProgress = false)
             toast(it)
         }.launchIn(viewLifecycleOwner.lifecycleScope)
     }
