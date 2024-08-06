ccf9130e4d6817b6f9db8f26584440ad8ba7d6e8
Akmalkon <akhanq61@gmail.cpm>
2024-08-06 11:22:22 +0500

    Name changed

--- app/src/main/java/uz/androbeck/virtualbank/ui/screens/bottom_nav_items/history/HistoryFragment.kt
+++ app/src/main/java/uz/androbeck/virtualbank/ui/screens/bottom_nav_items/history/HistoryFragment.kt
@@ -4,6 +4,7 @@ import uz.androbeck.virtualbank.R
 import uz.androbeck.virtualbank.ui.base.BaseFragment
 
 class HistoryFragment : BaseFragment(R.layout.fragment_history) {
+
     override fun setup() {
 
     }
