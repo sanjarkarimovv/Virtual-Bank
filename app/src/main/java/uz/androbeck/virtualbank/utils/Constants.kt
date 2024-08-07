75fc99d71513166f97c6374be13e3d366002f042
jalol <jalol2510@gmail.com>
2024-08-08 00:10:11 +0500

    Create Custom dialog

--- app/src/main/java/uz/androbeck/virtualbank/utils/Constants.kt
+++ app/src/main/java/uz/androbeck/virtualbank/utils/Constants.kt
@@ -3,5 +3,6 @@ package uz.androbeck.virtualbank.utils
 object Constants {
     object FileName {
         const val SHARED_PREFS = "virtual_bank"
+
     }
 }
--- app/src/main/java/uz/androbeck/virtualbank/utils/adapters/ChecklistAdapter.kt
+++ app/src/main/java/uz/androbeck/virtualbank/utils/adapters/ChecklistAdapter.kt
@@ -1,4 +1,4 @@
-package uz.androbeck.virtualbank.adapters
+package uz.androbeck.virtualbank.utils.adapters
 
 import android.view.LayoutInflater
 import android.view.View
