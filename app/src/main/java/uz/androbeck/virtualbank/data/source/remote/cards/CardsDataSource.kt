1422192bdf7d8a3099ecdb3a409a4953bb59b6a0
Maxfiybek <mirjamolq828@gmail.com>
2024-08-12 04:33:10 +0500

    Full Info Api and UserFullInfoPage finished

--- app/src/main/java/uz/androbeck/virtualbank/data/source/remote/cards/CardsDataSource.kt
+++
@@ -1,8 +0,0 @@
-package uz.androbeck.virtualbank.data.source.remote.cards
-
-import kotlinx.coroutines.flow.Flow
-import uz.androbeck.virtualbank.data.dto.response.cards.GetCardsResDto
-
-interface CardsDataSource {
-    suspend fun getCards(): Flow<GetCardsResDto>
-}
--- app/src/main/java/uz/androbeck/virtualbank/data/source/remote/cards/CardsDataSourceImpl.kt
+++
@@ -1,9 +0,0 @@
-package uz.androbeck.virtualbank.data.source.remote.cards
-
-import kotlinx.coroutines.flow.flow
-
-class CardsDataSourceImpl(
-    private val service: CardsService
-) : CardsDataSource {
-    override suspend fun getCards() = flow { emit(service.getCards()) }
-}
