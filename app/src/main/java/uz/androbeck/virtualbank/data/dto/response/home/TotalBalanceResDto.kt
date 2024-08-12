25d3a73417308bc104b393a5b85bb77d9865dc75
jalol <jalol2510@gmail.com>
2024-08-12 16:57:30 +0500

    Serializable annotation added

--- app/src/main/java/uz/androbeck/virtualbank/data/dto/response/home/TotalBalanceResDto.kt
+++ app/src/main/java/uz/androbeck/virtualbank/data/dto/response/home/TotalBalanceResDto.kt
@@ -1,7 +1,9 @@
 package uz.androbeck.virtualbank.data.dto.response.home
 
 import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class TotalBalanceResDto(
     @SerialName("total-balance")
     val total_balance: String? = null,
