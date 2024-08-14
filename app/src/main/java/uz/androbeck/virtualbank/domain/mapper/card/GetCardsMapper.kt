8dc371d906c0f86c3b6335aacf4b8e9bd0178a8c
User <abbosbeknormuratov2003@gmail.com>
2024-08-15 00:26:19 +0500

    kjsahdjhjdahkj

--- app/src/main/java/uz/androbeck/virtualbank/domain/mapper/card/GetCardsMapper.kt
+++ app/src/main/java/uz/androbeck/virtualbank/domain/mapper/card/GetCardsMapper.kt
@@ -7,8 +7,23 @@ import uz.androbeck.virtualbank.domain.ui_models.cards.GetCardsUIModel
 import javax.inject.Inject
 
 class GetCardsMapper @Inject constructor(): BaseMapper<GetCardsResDto, GetCardsUIModel> {
-    override fun toUIModel(dto: GetCardsResDto): GetCardsUIModel {
-
+    override fun toUIModel(dto: GetCardsResDto) = dto.run {
+        GetCardsUIModel(
+            getCardsUIModel = dto.map {
+                CardUIModel(
+                    id = dto.id,
+                    name = it.name,
+                    amount = it.amount,
+                    owner = it.owner,
+                    pan = it.pan,
+                    expiredYear = it.expiredYear,
+                    expiredMonth = it.expiredMonth,
+                    themeType = it.themeType,
+                    isVisible = it.isVisible
+                )
+            }
+        )
+    }
 
     override fun toDTO(uiModel: GetCardsUIModel): GetCardsResDto {
         TODO("Not yet implemented")
