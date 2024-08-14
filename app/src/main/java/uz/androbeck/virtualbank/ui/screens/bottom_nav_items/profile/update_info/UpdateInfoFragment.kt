4ca4e6fb1b91016b8082c84d0d6f9030bc86489c
Maxfiybek <mirjamolq828@gmail.com>
2024-08-15 02:48:17 +0500

    Full Info Api and UserFullInfoPage finished

--- app/src/main/java/uz/androbeck/virtualbank/ui/screens/bottom_nav_items/profile/update_info/UpdateInfoFragment.kt
+++ app/src/main/java/uz/androbeck/virtualbank/ui/screens/bottom_nav_items/profile/update_info/UpdateInfoFragment.kt
@@ -1,7 +1,18 @@
 package uz.androbeck.virtualbank.ui.screens.bottom_nav_items.profile.update_info
 
+import by.kirich1409.viewbindingdelegate.viewBinding
 import uz.androbeck.virtualbank.R
+import uz.androbeck.virtualbank.databinding.FragmentUpdateInfoBinding
 import uz.androbeck.virtualbank.ui.base.BaseFragment
 
-class UpdateInfoFragment : BaseFragment(R.layout) {
+class UpdateInfoFragment : BaseFragment(R.layout.fragment_update_info) {
+    private val binding by viewBinding(FragmentUpdateInfoBinding::bind)
+
+    override fun setup() {
+
+    }
+
+    override fun clicks() {
+
+    }
 }
