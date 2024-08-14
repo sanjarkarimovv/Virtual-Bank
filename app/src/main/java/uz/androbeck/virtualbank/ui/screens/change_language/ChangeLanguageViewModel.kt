bc32070ececac10cde9dd9cd2c22f7ef474dcd1f
sanjarkarimov <sanjarkarimov1996@mail.ru>
2024-08-14 19:01:21 +0500

    BottomSheet Language Done

--- app/src/main/java/uz/androbeck/virtualbank/ui/screens/change_language/ChangeLanguageViewModel.kt
+++ app/src/main/java/uz/androbeck/virtualbank/ui/screens/change_language/ChangeLanguageViewModel.kt
@@ -4,11 +4,13 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.consumeAsFlow
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
 import uz.androbeck.virtualbank.preferences.PreferencesProvider
 import uz.androbeck.virtualbank.ui.enums.Language
 import uz.androbeck.virtualbank.utils.extentions.getLanguageByCode
+import uz.androbeck.virtualbank.utils.extentions.share
 import javax.inject.Inject
 
 
@@ -18,7 +20,7 @@ class ChangeLanguageViewModel @Inject constructor(
 ) : ViewModel() {
 
     private val _language = Channel<Language>()
-    val language = _language.receiveAsFlow()
+    val language = _language.consumeAsFlow().share(viewModelScope)
 
     fun setLanguage(language: String) = viewModelScope.launch {
         if (language != preferencesProvider.language)
