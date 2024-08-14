f8647ee959a4c09597ba62448810a23ac8afda11
Maxfiybek <mirjamolq828@gmail.com>
2024-08-15 02:40:36 +0500

    Full Info Api and UserFullInfoPage finished

---
+++ app/src/main/java/uz/androbeck/virtualbank/ui/screens/bottom_nav_items/profile/update_info/UpdateInfoFragment.kt
@@ -0,0 +1,7 @@
+package uz.androbeck.virtualbank.ui.screens.bottom_nav_items.profile.update_info
+
+import uz.androbeck.virtualbank.R
+import uz.androbeck.virtualbank.ui.base.BaseFragment
+
+class UpdateInfoFragment : BaseFragment(R.layout) {
+}
