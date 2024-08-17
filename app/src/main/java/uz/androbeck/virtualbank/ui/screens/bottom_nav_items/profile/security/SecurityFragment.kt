0de0e081a223afc0866ab72cc33f0eed296878e8
dilshodbekqudratov <dilshodqudratov522@gmail.com>
2024-08-17 19:32:28 +0500

    Security Setting init done

--- app/src/main/java/uz/androbeck/virtualbank/ui/screens/bottom_nav_items/profile/security/SecurityFragment.kt
+++ app/src/main/java/uz/androbeck/virtualbank/ui/screens/bottom_nav_items/profile/security/SecurityFragment.kt
@@ -7,35 +7,23 @@ import by.kirich1409.viewbindingdelegate.viewBinding
 import dagger.hilt.android.AndroidEntryPoint
 import uz.androbeck.virtualbank.R
 import uz.androbeck.virtualbank.databinding.FragmentSecurityBinding
-import uz.androbeck.virtualbank.preferences.PreferencesProvider
 import uz.androbeck.virtualbank.ui.base.BaseFragment
 import uz.androbeck.virtualbank.ui.screens.bottom_nav_items.profile.security.enums.SecuritySettingKey
-import javax.inject.Inject
+import uz.androbeck.virtualbank.ui.screens.bottom_nav_items.profile.security.model.SecuritySettings
+import uz.androbeck.virtualbank.utils.extentions.singleClickable
 
 @AndroidEntryPoint
 class SecurityFragment :BaseFragment(R.layout.fragment_security) {
     private val binding by viewBinding(FragmentSecurityBinding::bind)
     private val viewModel by viewModels<SecurityViewModel>()
     override fun setup() {
-
-    }
-
-    private fun updateUI() = with(binding) {
-        switchBiometrics.isChecked
     }
 
     override fun observe() {
         viewModel.apply {
             securitySettings.observe(viewLifecycleOwner) {
-                it.run {
-                    with(binding) {
-                        switchBiometrics.isChecked = useBiometric
-                        switchPayment.isChecked = useBiometricPayment
-                        switchLock.isChecked = useIsAwayLong
-                        seekBarTime.progress = awayLongTime
-                        selectedTimeText.text = requireContext().getString(R.string.str_security_time, awayLongTime)
-                    }
-                }
+                updateUI(it)
+                println(it.toString())
             }
         }
     }
@@ -44,20 +32,58 @@ class SecurityFragment :BaseFragment(R.layout.fragment_security) {
         securityToolbar.setNavigationOnClickListener {
             findNavController().navigate(R.id.action_securityFragment_to_profileFragment)
         }
+
         seekBarTime.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
                 selectedTimeText.text = requireContext().getString(R.string.str_security_time, progress)
-                viewModel.updateSetting(SecuritySettingKey.AWAY_LONG_TIME, progress)
-                println(progress
-                )
             }
 
             override fun onStartTrackingTouch(seekBar: SeekBar?) {
             }
 
             override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                viewModel.updateSetting(SecuritySettingKey.AWAY_LONG_TIME, seekBar?.progress ?: 0)
             }
         })
+
+        biometricSettings.singleClickable{
+            switchBiometrics.isChecked = !switchBiometrics.isChecked
+            viewModel.updateSetting(SecuritySettingKey.USE_BIOMETRIC, switchBiometrics.isChecked)
+        }
+
+        switchBiometrics.setOnCheckedChangeListener{
+                _, isChecked ->
+            viewModel.updateSetting(SecuritySettingKey.USE_BIOMETRIC, isChecked)
+        }
+
+        paymentSettings.singleClickable{
+            switchPayment.isChecked = !switchPayment.isChecked
+            viewModel.updateSetting(SecuritySettingKey.USE_BIOMETRIC_PAYMENT, switchPayment.isChecked)
+        }
+
+        switchPayment.setOnCheckedChangeListener{
+                _, isChecked ->
+            viewModel.updateSetting(SecuritySettingKey.USE_BIOMETRIC_PAYMENT, isChecked)
+        }
+
+        lockSettings.singleClickable{
+            switchLock.isChecked = !switchLock.isChecked
+            viewModel.updateSetting(SecuritySettingKey.USE_IS_AWAY_LONG, switchLock.isChecked)
+        }
+
+        switchLock.setOnCheckedChangeListener{
+                _, isChecked ->
+            viewModel.updateSetting(SecuritySettingKey.USE_IS_AWAY_LONG, isChecked)
+        }
     }
 
+    private fun updateUI(settings: SecuritySettings) = with(binding) {
+        settings.run {
+                switchBiometrics.isChecked = useBiometric
+                switchPayment.isChecked = useBiometricPayment
+                switchLock.isChecked = useIsAwayLong
+                seekBarTime.progress = awayLongTime
+                selectedTimeText.text = requireContext().getString(R.string.str_security_time, awayLongTime)
+        }
+    }
 }
