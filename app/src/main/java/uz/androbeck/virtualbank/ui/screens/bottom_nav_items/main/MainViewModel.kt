796a1646c3623f8e001d38816fddc36f016c0181
User <abbosbeknormuratov2003@gmail.com>
2024-08-19 18:47:43 +0500

    Transfer API called

--- app/src/main/java/uz/androbeck/virtualbank/ui/screens/bottom_nav_items/main/MainViewModel.kt
+++ app/src/main/java/uz/androbeck/virtualbank/ui/screens/bottom_nav_items/main/MainViewModel.kt
@@ -16,24 +16,6 @@ import javax.inject.Inject
 class MainViewModel @Inject constructor(
     private val getFullInfoUseCase: GetFullInfoUseCase,
     private val putUpdateInfoUseCase: PutUpdateInfoUseCase,
-    private val transfersUseCase: TransferUseCase,
 ) : ViewModel() {
-    fun aa() {
-        println("start")
-        transfersUseCase.invoke(
-            uiReqModel = TransferUIModel(
-                "third-card",
-                "7",
-                "1234567898765432",
-                100100.0
-            )
-        ).onEach {
-
-            println(it.token)
-        }.catch {
-            println(it.message)
-        }
-            .launchIn(viewModelScope)
-    }
 
 }
