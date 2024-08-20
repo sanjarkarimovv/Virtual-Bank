6f16dea62126d9b1086371de7e54617ef088d03c
Akmalkon <akhanq61@gmail.cpm>
2024-08-20 12:00:49 +0500

    change

--- app/src/main/java/uz/androbeck/virtualbank/ui/screens/bottom_nav_items/transfer/TransferFragment.kt
+++ app/src/main/java/uz/androbeck/virtualbank/ui/screens/bottom_nav_items/transfer/TransferFragment.kt
@@ -27,7 +27,7 @@ class TransferFragment : BaseFragment(R.layout.fragment_transfer) {
         adjustButtonPositionForKeyboard()
         with(binding) {
             etCardOrPhoneNumber.addTextChangedListener {
-                if (it?.length == 19||it?.length == 16)
+                if (it?.length == 19 || it?.length == 16)
                     btnContinue.isEnable = true
                 else
                     btnContinue.isEnable = false
@@ -48,7 +48,8 @@ class TransferFragment : BaseFragment(R.layout.fragment_transfer) {
                 mcvTransferHimself.visibility = View.VISIBLE
                 mcvGallery.visibility = View.VISIBLE
                 mcvTransferContacts.visibility = View.VISIBLE
-                val imm = requireContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                val imm =
+                    requireContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
                 imm.hideSoftInputFromWindow(binding.etCardOrPhoneNumber.windowToken, 0)
             }
 
@@ -87,28 +88,48 @@ class TransferFragment : BaseFragment(R.layout.fragment_transfer) {
         setupOnBackPressed()
     }
 
+    //    private fun adjustButtonPositionForKeyboard() {
+//        val rootView = requireActivity().window.decorView.findViewById<View>(android.R.id.content)
+//        rootView.viewTreeObserver.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
+//            override fun onGlobalLayout() {
+//                val rect = Rect()
+//                rootView.getWindowVisibleDisplayFrame(rect)
+//                val screenHeight = rootView.height
+//
+//                val keypadHeight = screenHeight - rect.bottom
+//
+//                // If the keyboard is open
+//                if (keypadHeight > screenHeight * 0.15) {
+//                    binding.btnContinue.visibility = View.VISIBLE
+//                    // Move the button above the keyboard
+//                    binding.btnContinue.translationY = -keypadHeight.toFloat() + binding.btnContinue.height
+//                } else {
+//                    // Reset the button position when the keyboard is hidden
+//                    binding.btnContinue.translationY = 0f
+//                    binding.btnContinue.visibility = View.GONE
+//                }
+//            }
+//        })
+//    }
+    private val onGlobalLayoutListener = ViewTreeObserver.OnGlobalLayoutListener {
+        val rect = Rect()
+        val rootView = requireActivity().window.decorView.findViewById<View>(android.R.id.content)
+        rootView.getWindowVisibleDisplayFrame(rect)
+        val screenHeight = rootView.height
+
+        val keypadHeight = screenHeight - rect.bottom
+
+        if (keypadHeight > screenHeight * 0.15) {
+            binding.btnContinue.visibility = View.VISIBLE
+            binding.btnContinue.translationY = -keypadHeight.toFloat() + binding.btnContinue.height
+        } else {
+            binding.btnContinue.translationY = 0f
+        }
+    }
+
     private fun adjustButtonPositionForKeyboard() {
         val rootView = requireActivity().window.decorView.findViewById<View>(android.R.id.content)
-        rootView.viewTreeObserver.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
-            override fun onGlobalLayout() {
-                val rect = Rect()
-                rootView.getWindowVisibleDisplayFrame(rect)
-                val screenHeight = rootView.height
-
-                val keypadHeight = screenHeight - rect.bottom
-
-                // If the keyboard is open
-                if (keypadHeight > screenHeight * 0.15) {
-                    binding.btnContinue.visibility = View.VISIBLE
-                    // Move the button above the keyboard
-                    binding.btnContinue.translationY = -keypadHeight.toFloat() + binding.btnContinue.height
-                } else {
-                    // Reset the button position when the keyboard is hidden
-                    binding.btnContinue.translationY = 0f
-                    binding.btnContinue.visibility = View.GONE
-                }
-            }
-        })
+        rootView.viewTreeObserver.addOnGlobalLayoutListener(onGlobalLayoutListener)
     }
 
 
@@ -118,22 +139,32 @@ class TransferFragment : BaseFragment(R.layout.fragment_transfer) {
         etCardNumber?.onActivityResult(requestCode, resultCode, data)
 
     }
+
     private fun setupOnBackPressed() {
-        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, object : OnBackPressedCallback(true) {
-            override fun handleOnBackPressed() {
-                if (binding.etCardOrPhoneNumber.hasFocus()) {
-                    binding.etCardOrPhoneNumber.clearFocus()
-                    binding.btnContinue.visibility = View.GONE
-                    binding.ivBack.visibility = View.GONE
-                    binding.mcvTransferHimself.visibility = View.VISIBLE
-                    binding.mcvGallery.visibility = View.VISIBLE
-                    binding.mcvTransferContacts.visibility = View.VISIBLE
-                } else {
-                    // Вызовите метод родительской активности
-                    isEnabled = false // Чтобы не зациклить вызов
-                    requireActivity().onBackPressedDispatcher.onBackPressed()
+        requireActivity().onBackPressedDispatcher.addCallback(
+            viewLifecycleOwner,
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    if (binding.etCardOrPhoneNumber.hasFocus()) {
+                        binding.etCardOrPhoneNumber.clearFocus()
+                        binding.btnContinue.visibility = View.GONE
+                        binding.ivBack.visibility = View.GONE
+                        binding.mcvTransferHimself.visibility = View.VISIBLE
+                        binding.mcvGallery.visibility = View.VISIBLE
+                        binding.mcvTransferContacts.visibility = View.VISIBLE
+                    } else {
+                        // Вызовите метод родительской активности
+                        isEnabled = false // Чтобы не зациклить вызов
+                        requireActivity().onBackPressedDispatcher.onBackPressed()
+                    }
                 }
-            }
-        })
+            })
     }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        val rootView = requireActivity().window.decorView.findViewById<View>(android.R.id.content)
+        rootView.viewTreeObserver.removeOnGlobalLayoutListener(onGlobalLayoutListener)
+    }
+
 }
