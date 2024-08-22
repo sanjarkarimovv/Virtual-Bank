d89cd19a3a64f9b39c1c67ad6ed40d71c3f1f2b9
Maxfiybek <mirjamolq828@gmail.com>
2024-08-22 09:14:58 +0500

    Transfer Verify Created till the UseCase

--- app/src/main/java/uz/androbeck/virtualbank/ui/screens/bottom_nav_items/profile/ProfileFragment.kt
+++ app/src/main/java/uz/androbeck/virtualbank/ui/screens/bottom_nav_items/profile/ProfileFragment.kt
@@ -1,24 +1,19 @@
 package uz.androbeck.virtualbank.ui.screens.bottom_nav_items.profile
 
+import androidx.core.os.bundleOf
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.launch
 import uz.androbeck.virtualbank.R
-import uz.androbeck.virtualbank.data.dto.common.request.CodeVerifyReqDto
 import uz.androbeck.virtualbank.databinding.FragmentProfileBinding
-import uz.androbeck.virtualbank.preferences.PreferencesProvider
 import uz.androbeck.virtualbank.domain.ui_models.home.FullInfoUIModel
 import uz.androbeck.virtualbank.ui.base.BaseFragment
 import uz.androbeck.virtualbank.ui.dialogs.change_language.ChangeLanguageBottomDialog
 import uz.androbeck.virtualbank.utils.Constants
 import uz.androbeck.virtualbank.utils.extentions.singleClickable
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class ProfileFragment : BaseFragment(R.layout.fragment_profile) {
@@ -28,30 +23,6 @@ class ProfileFragment : BaseFragment(R.layout.fragment_profile) {
 
     override fun setup(): Unit {
         vm.getUserData()
-    }
-    @Inject
-    lateinit var prefs: PreferencesProvider
-
-    override fun setup() {
-        viewLifecycleOwner.lifecycleScope.launch {
-            println("loading...")
-            vm.transferVerifyApiCall(
-                CodeVerifyReqDto(
-                    prefs.token,
-                    "56216"
-                )
-            ).collect {
-                when (it) {
-                    is Event.Error -> {
-                        println("Error -> ${it.message.toString()}")
-                    }
-
-                    is Event.Success -> {
-                        println("Success -> ${it.message.toString()}")
-                    }
-                }
-            }
-        }
     }
 
     override fun clicks() = with(binding) {
