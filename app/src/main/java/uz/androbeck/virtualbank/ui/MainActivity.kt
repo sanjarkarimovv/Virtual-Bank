470a50156fc2af8124a930040d25dd955839b820
AndroidDeveloperSanjar <sanjarkarimov1996@mail.ru>
2024-08-22 10:31:06 +0500

    CHanged

--- app/src/main/java/uz/androbeck/virtualbank/ui/MainActivity.kt
+++ app/src/main/java/uz/androbeck/virtualbank/ui/MainActivity.kt
@@ -1,7 +1,6 @@
 package uz.androbeck.virtualbank.ui
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
@@ -20,7 +19,6 @@ import uz.androbeck.virtualbank.preferences.PreferencesProvider
 import uz.androbeck.virtualbank.ui.events.NavGraphEvent
 import uz.androbeck.virtualbank.utils.extentions.getLanguageByCode
 import uz.androbeck.virtualbank.utils.extentions.gone
-import uz.androbeck.virtualbank.utils.extentions.toast
 import uz.androbeck.virtualbank.utils.extentions.visible
 import java.util.Locale
 import javax.inject.Inject
@@ -122,7 +120,7 @@ class MainActivity : AppCompatActivity() {
     private fun defaultNavHostTrue(navHostFragment: NavHostFragment) {
         supportFragmentManager.beginTransaction().setPrimaryNavigationFragment(navHostFragment)
             .commit()
-    }////
+    }
 
     override fun onPause() {
         super.onPause()
