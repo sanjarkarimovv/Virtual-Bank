28c84fa06f4c216a85637efcec0a6f8742bb4a1d
Java <normurodov3ds@gmail.com>
2024-08-22 09:21:07 +0500

    home ui fixed, total balance api call

--- app/src/main/java/uz/androbeck/virtualbank/network/interceptors/AuthInterceptor.kt
+++ app/src/main/java/uz/androbeck/virtualbank/network/interceptors/AuthInterceptor.kt
@@ -1,11 +1,13 @@
 package uz.androbeck.virtualbank.network.interceptors
 
+import com.google.gson.Gson
 import kotlinx.coroutines.runBlocking
 import okhttp3.Interceptor
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.RequestBody.Companion.toRequestBody
 import okhttp3.Response
 import uz.androbeck.virtualbank.BuildConfig
 import uz.androbeck.virtualbank.data.dto.common.response.Tokens2ResDto
-import uz.androbeck.virtualbank.data.dto.common.response.TokensResDto
 import uz.androbeck.virtualbank.preferences.PreferencesProvider
 import javax.inject.Inject
 import javax.inject.Singleton
