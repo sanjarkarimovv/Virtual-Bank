4f0a62df3c3072f03e5e0f735ac1956858bed63d
User <abbosbeknormuratov2003@gmail.com>
2024-08-23 11:16:26 +0500

    card_options

---
+++ app/src/main/java/uz/androbeck/virtualbank/ui/screens/requisition/RequisitionFragment.kt
@@ -0,0 +1,12 @@
+package uz.androbeck.virtualbank.ui.screens.requisition
+
+import uz.androbeck.virtualbank.R
+import uz.androbeck.virtualbank.ui.base.BaseFragment
+
+class RequisitionFragment:BaseFragment(R.layout.fragment_requisition) {
+    override fun setup() {
+
+
+
+    }
+}
