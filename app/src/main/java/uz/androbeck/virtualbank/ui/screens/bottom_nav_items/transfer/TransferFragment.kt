b6b5f5ce00b1fa6589cbc42330f6c2841b8bd30d
jalol <jalol2510@gmail.com>
2024-08-23 18:24:44 +0500

    Transfer to my card

--- app/src/main/java/uz/androbeck/virtualbank/ui/screens/bottom_nav_items/transfer/TransferFragment.kt
+++ app/src/main/java/uz/androbeck/virtualbank/ui/screens/bottom_nav_items/transfer/TransferFragment.kt
@@ -1,19 +1,10 @@
 package uz.androbeck.virtualbank.ui.screens.bottom_nav_items.transfer
 
-import android.util.Log
-import androidx.fragment.app.viewModels
-import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.onEach
 import uz.androbeck.virtualbank.R
-import uz.androbeck.virtualbank.domain.ui_models.transfer.GetFeeReqUIModel
-import uz.androbeck.virtualbank.network.message.MessageController
 import uz.androbeck.virtualbank.ui.base.BaseFragment
-import javax.inject.Inject
 
-@AndroidEntryPoint
 class TransferFragment : BaseFragment(R.layout.fragment_transfer) {
     override fun setup() {
 
     }
-
 }
